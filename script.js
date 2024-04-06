411643ad069bb6e4ceb8aa5e39102ca2d4e923d9
Dnyaneshwar Magar <89983890+dnyaneshwarmagar@users.noreply.github.com>
2024-04-06 08:00:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+let myForm = document.querySelector("form");
+	
+
+myForm.addEventListener("submit",(e)=>{
+	e.preventDefault();
+	let name = document.getElementById("name").value;
+let age = document.getElementById("age").value;
+
+	return new Promise((resolve,reject)=>{
+		let msg = ""
+		
+		if(+age >18 ){
+			msg = `Welcome, ${name}. You can vote.`
+		}
+		else{
+			msg = `Oh sorry ${name}. You aren't old enough.`
+		}
+		
+		setTimeout(()=>{resolve(); alert(msg)},4000)
+})
+})
